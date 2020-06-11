48fa7abb6353be447095c33a9fc09b1a3015c4a9
fernandocou <63759935+fernandocou@users.noreply.github.com>
2020-06-10 21:50:15 -0300

    Add files via upload

---
+++ AulaWebApi/AulaWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AulaWebApi.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApi.Services;
+
+namespace AulaWebApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IUserService _userservice;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IUserService userservice
+{ 
+            _logger = logger;
+        _userservice = userservice ?? throw new ArgumentNullException(nameof(userservice));
+
+        }
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = _userservice.Authenticate(model.Username, model.Password);
+            
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+    }
+}
---
+++ AulaWebApi/AulaWebApi/Helpers/AppSttings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaWebApi.Helpers
+{
+    public class AppSttings
+    {
+        public string Secret { get; set; }
+    }
+}
